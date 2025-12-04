88b44e5c4182793e57836f74c1842cd3af0f90e8
Md. Nazmul Hossain Shishir <nazmul.hossain.shishir14@gmail.com>
2025-12-04 23:42:20 +0600

    home page base built

--- app/page.js
+++ app/page.js
@@ -1,16 +1,10 @@
+import Title from "@/components/Home/Title";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center p-6 bg-white text-black">
-      <h1 className="text-4xl font-bold mb-4">
-        Welcome to NovaStack Technologies
-      </h1>
-      <p className="text-lg text-center max-w-2xl">
-        We are committed to delivering innovative tech solutions that drive
-        success and transform businesses. Explore our services and discover how
-        we can help you achieve your goals.
-      </p>
+    <div className="flex flex-col items-center w-full">
+      <Title />
     </div>
   );
 }
---
+++ components/Home/Title.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div className="flex flex-col items-center p-6 bg-white text-black w-full">
+      <h1 className="text-4xl font-bold mb-4">
+        Welcome to NovaStack Technologies
+      </h1>
+      <p className="text-lg text-center max-w-2xl">
+        We are committed to delivering innovative tech solutions that drive
+        success and transform businesses. Explore our services and discover how
+        we can help you achieve your goals.
+      </p>
+    </div>
+  );
+}
+
+export default Title;
