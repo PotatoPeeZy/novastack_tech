cd519c685705ac2da8eade588a8fed70e122401b
Md. Nazmul Hossain Shishir <nazmul.hossain.shishir14@gmail.com>
2025-12-04 19:17:18 +0600

    moved the footer sections into seperate files

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
@@ -20,7 +21,7 @@ const Footer = () => {
 
           {/* Social Icons */}
           <div className="flex flex-row gap-4 mt-4">
-            <a
+            <Link
               href="https://facebook.com/NovaStackTech"
               target="_blank"
               rel="noopener noreferrer"
@@ -32,8 +33,8 @@ const Footer = () => {
                 height={24}
                 className="icon-hover"
               />
-            </a>
-            <a
+            </Link>
+            <Link
               href="https://linkedin.com/company/novastack-tech"
               target="_blank"
               rel="noopener noreferrer"
@@ -45,8 +46,8 @@ const Footer = () => {
                 height={24}
                 className="icon-hover"
               />
-            </a>
-            <a
+            </Link>
+            <Link
               href="https://github.com/NovaStack-Tech"
               target="_blank"
               rel="noopener noreferrer"
@@ -58,7 +59,7 @@ const Footer = () => {
                 height={24}
                 className="icon-hover"
               />
-            </a>
+            </Link>
           </div>
         </div>
 
