d4dde4653e3f0bf4f37f846533915b3d0c3d9552
Md. Nazmul Hossain Shishir <nazmul.hossain.shishir14@gmail.com>
2025-12-04 19:58:18 +0600

    completed the footer section

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FooterSec1 from "./FooterSections/FooterSec1";
 import FooterSec2 from "./FooterSections/FooterSec2";
+import FooterSec3 from "./FooterSections/FooterSec3";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -9,16 +10,17 @@ const Footer = () => {
     <footer className="mx-auto px-6 py-4 border-t bg-white text-black justify-center items-center flex">
       <div
         style={{ maxWidth: "80rem" }}
-        className="flex flex-col sm:flex-row sm:justify-between w-full"
+        className="flex flex-col gap-4 justify-center items-center sm:gap-0 sm:flex-row sm:justify-between w-full"
       >
-        <div className="max-w-[20%]">
+        <div className="w-full sm:max-w-[30%]">
           <FooterSec1 />
         </div>
-        <div className="max-w-[20%]">
+        <div className="w-full sm:max-w-[30%]">
           <FooterSec2 />
         </div>
-        <div>sec1</div>
-        <div>sec1</div>
+        <div className="w-full sm:max-w-[30%]">
+          <FooterSec3 />
+        </div>
       </div>
     </footer>
   );
--- components/Footer/FooterSections/FooterSec3.js
+++ components/Footer/FooterSections/FooterSec3.js
@@ -1,7 +1,37 @@
+import Link from "next/link";
 import React from "react";
 
 function FooterSec3() {
-  return <div>FooterSec3</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="text-lg text-[color:var(--clr-primary)]">Contact</div>
+      <div className="text-sm">
+        Phone:{" "}
+        <Link
+          href="tel:01792438135"
+          className="hover:text-[var(--clr-primary)] transition"
+        >
+          +880-1792-438135
+        </Link>
+      </div>
+      <div className="text-sm">
+        <Link
+          className="hover:text-[var(--clr-primary)] transition"
+          href="mailto:"
+        >
+          contact@novastack.tech
+        </Link>
+      </div>
+      <div className="text-sm">
+        <Link
+          className="hover:text-[var(--clr-primary)] transition"
+          href="https://maps.google.com"
+        >
+          Address: 123 Software St, Khulna, Bangladesh
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default FooterSec3;
