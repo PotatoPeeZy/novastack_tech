a02052acfc3c42dd913250be7066a59e849b0890
Md. Nazmul Hossain Shishir <nazmul.hossain.shishir14@gmail.com>
2025-12-04 18:40:00 +0600

    completed first section on the footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
@@ -9,10 +10,16 @@ const Footer = () => {
         style={{ maxWidth: "80rem" }}
         className="flex flex-col sm:flex-row sm:justify-between w-full"
       >
+        <div className="flex flex-col items-center max-w-[25%] font-light text-justify">
+          <Image src="/logo.png" alt="Logo" width={48} height={48} />
+          <div className="text-[var(--clr-primary)] text-2xl">NovaStack</div>
+          <div className="text-s">
+            NovaStack Technology based software firm with the main services and
+            development department in khulna, Bangladesh
+          </div>
+        </div>
         <div>sec1</div>
-        <div>sec1</div>
-        <div>sec1</div>
-        <div>sec1</div>
+        <div>sec1</div>s
       </div>
     </footer>
   );
