fa90418b80dfe4d81189634e74ed9fccac8323b9
Md. Nazmul Hossain Shishir <nazmul.hossain.shishir14@gmail.com>
2025-12-04 19:17:36 +0600

    2

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
+import FooterSec1 from "./FooterSec1";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -11,58 +10,10 @@ const Footer = () => {
         style={{ maxWidth: "80rem" }}
         className="flex flex-col sm:flex-row sm:justify-between w-full"
       >
-        <div className="flex flex-col items-center max-w-[20%]">
-          <Image src="/logo.png" alt="Logo" width={48} height={48} />
-          <div className="text-[var(--clr-primary)] text-2xl">NovaStack</div>
-          <div className="font-extralight text-justify text-sm mt-2">
-            NovaStack Technology is a software firm with the main services and
-            development department in Khulna, Bangladesh.
-          </div>
-
-          {/* Social Icons */}
-          <div className="flex flex-row gap-4 mt-4">
-            <Link
-              href="https://facebook.com/NovaStackTech"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                src="/icons/facebook.svg"
-                alt="Facebook"
-                width={24}
-                height={24}
-                className="icon-hover"
-              />
-            </Link>
-            <Link
-              href="https://linkedin.com/company/novastack-tech"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                src="/icons/linkedin.svg"
-                alt="LinkedIn"
-                width={24}
-                height={24}
-                className="icon-hover"
-              />
-            </Link>
-            <Link
-              href="https://github.com/NovaStack-Tech"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                src="/icons/github.svg"
-                alt="GitHub"
-                width={24}
-                height={24}
-                className="icon-hover"
-              />
-            </Link>
-          </div>
+        <div className="max-w-[20%]">
+          <FooterSec1 />
         </div>
-
+        <div>sec1</div>
         <div>sec1</div>
         <div>sec1</div>
       </div>
