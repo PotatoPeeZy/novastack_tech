4f65990ba4c0533b09457f819508de305397ba08
Md. Nazmul Hossain Shishir <nazmul.hossain.shishir14@gmail.com>
2025-12-04 19:04:13 +0600

    added some style on the icons

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -10,47 +10,58 @@ const Footer = () => {
         style={{ maxWidth: "80rem" }}
         className="flex flex-col sm:flex-row sm:justify-between w-full"
       >
-        <div className="flex flex-col items-center max-w-[20%] ">
+        <div className="flex flex-col items-center max-w-[20%]">
           <Image src="/logo.png" alt="Logo" width={48} height={48} />
           <div className="text-[var(--clr-primary)] text-2xl">NovaStack</div>
           <div className="font-extralight text-justify text-sm mt-2">
             NovaStack Technology is a software firm with the main services and
-            development department in khulna, Bangladesh
+            development department in Khulna, Bangladesh.
           </div>
+
+          {/* Social Icons */}
           <div className="flex flex-row gap-4 mt-4">
             <a
-              href="https://facebook.com"
+              href="https://facebook.com/NovaStackTech"
               target="_blank"
               rel="noopener noreferrer"
             >
               <Image
-                src="/facebook.png"
+                src="/icons/facebook.svg"
                 alt="Facebook"
                 width={24}
                 height={24}
+                className="icon-hover"
               />
             </a>
             <a
-              href="https://linkedin.com"
+              href="https://linkedin.com/company/novastack-tech"
               target="_blank"
               rel="noopener noreferrer"
             >
               <Image
-                src="/linkedin.png"
+                src="/icons/linkedin.svg"
                 alt="LinkedIn"
                 width={24}
                 height={24}
+                className="icon-hover"
               />
             </a>
             <a
-              href="https://github.com"
+              href="https://github.com/NovaStack-Tech"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image src="/github.png" alt="GitHub" width={24} height={24} />
+              <Image
+                src="/icons/github.svg"
+                alt="GitHub"
+                width={24}
+                height={24}
+                className="icon-hover"
+              />
             </a>
           </div>
         </div>
+
         <div>sec1</div>
         <div>sec1</div>
       </div>
