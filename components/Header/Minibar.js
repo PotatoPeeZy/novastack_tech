7f7612263e26a6b7de341ae615fec511c4f598a0
Md. Nazmul Hossain Shishir <nazmul.hossain.shishir14@gmail.com>
2025-12-04 16:37:16 +0600

    added some styles to the minibar

--- components/Header/Minibar.js
+++ components/Header/Minibar.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 import React from "react";
 
 function Minibar() {
-  const linkClasses = "transition hover:text-[rgb(1,58,70)]";
+  const linkClasses = "hidden md:block transition hover:text-[rgb(1,58,70)]";
 
   return (
     <div className="flex flex-row h-3 text-sm font-extralight gap-4 justify-end items-center p-3">
       <Link className={linkClasses} href={"tel:01792438135"}>
         Call: +880-1792-438135
       </Link>
-      <Link href={"mailto:contact@novastack.tech"}>
+      <Link className={linkClasses} href={"mailto:contact@novastack.tech"}>
         Mail: contact@novastack.tech
       </Link>
     </div>
